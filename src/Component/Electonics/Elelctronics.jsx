c6854caed4575b71abc7180e0d6c0891f666aa79
ruthiedel <140281471+ruthiedel@users.noreply.github.com>
2024-10-28 12:05:27 +0200

    localStorage electronics

--- src/Component/Electonics/Elelctronics.jsx
+++ src/Component/Electonics/Elelctronics.jsx
@@ -3,25 +3,48 @@ import { fetchElectricity } from '../../Service/fakestoreCategories'
 import Styles from "./Elelctronics.module.css"
 function Elelctronics() {
 
-const [data,setData]= React.useState([])
+    const [data, setData] = React.useState([])
 
-React.useEffect(() => {
-    fetchElectricity()
-     .then(res => res.json()) 
-   .then(res => setData(res))
-}, [])
-  return (<>
-    <h1>Electronics</h1>
-    <div className={Styles.container}>
-      
-        {data.map((item) => (
-            <div key={item.id} className={Styles.item}>
-                <h2>{item.name}</h2>
-                <p>{item.description}</p>
-                <img src={item.image} alt={item.title} />
-            </div>
-        ))}
-    </div>
+    React.useEffect(() => {
+        let data = localStorage.getItem("Electronics"); // שימו לב לשם המפתח
+        let fiveMinutesAgo = null;
+        let storedDate = null;
+        const timeString = localStorage.getItem("Time");
+    
+        if (timeString) {
+            storedDate = new Date(timeString);
+            
+            const currentDate = new Date();
+            fiveMinutesAgo = new Date(currentDate.getTime() - 5 * 60 * 1000); 
+        }
+    
+        // בדיקת השוואה
+        if (data && timeString && storedDate && storedDate.getTime() >= fiveMinutesAgo.getTime()) {
+            setData(JSON.parse(data)); // עדכון הנתונים מ-localStorage
+        } else {
+            // אם אין נתונים תקינים, קרא ל-fetchElectricity
+            fetchElectricity()
+                .then(res => res.json())
+                .then(res => {
+                    setData(res); // עדכון הנתונים
+                    localStorage.setItem('Electronics', JSON.stringify(res)); 
+                    localStorage.setItem('Time', new Date().toISOString()); 
+                });
+        }
+    }, []);
+    
+    return (<>
+        <h1>Electronics</h1>
+        <div className={Styles.container}>
+
+            {data.map((item) => (
+                <div key={item.id} className={Styles.item}>
+                    <h2>{item.name}</h2>
+                    <p>{item.description}</p>
+                    <img src={item.image} alt={item.title} />
+                </div>
+            ))}
+        </div>
     </>)
 }
 
